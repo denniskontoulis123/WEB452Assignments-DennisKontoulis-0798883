1a09a008332519d3392ad7771f0032810da7feaf
denniskontoulis123 <w0798883@myscc.ca>
2023-10-09 10:36:03 -0400

    starting over again, keep getting the same error

--- MVCAssignment/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MVCAssignment.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
