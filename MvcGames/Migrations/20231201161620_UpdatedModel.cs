5e455a07f8a4aedfd58b7390f51ab50d41009766
denniskontoulis123 <w0798883@myscc.ca>
2023-12-01 11:19:05 -0500

    search functionality is basic, need to upgrade it

---
+++ MvcGames/Migrations/20231201161620_UpdatedModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcGames.Migrations
+{
+    public partial class UpdatedModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Developer",
+                table: "Games",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Games",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Developer",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Games");
+        }
+    }
+}
